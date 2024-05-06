bd496fc3b09a1d16e00d5a78b54e646e6227e0bc
devfle <52854338+devfle@users.noreply.github.com>
2024-05-06 19:47:37 +0200

    feat: add first form elements to feedback page

--- pages/Feedback.js
+++ pages/Feedback.js
@@ -1,7 +1,44 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
+import { Appbar, TextInput, Button } from 'react-native-paper';
 
 function Feedback() {
-  return <Text>Feedback Page</Text>;
+  return (
+    <>
+      <Appbar.Header mode="small">
+        <Appbar.Content title="Feedback" />
+      </Appbar.Header>
+
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.infoText}>This page is for employees only. For this reason it is also password protected. Employees can view your feedback here.</Text>
+        <TextInput style={styles.input} label="Username"></TextInput>
+        <TextInput secureTextEntry style={styles.input} label="Password"></TextInput>
+        <Button style={styles.sendButton} icon="login" mode="contained">
+          Login
+        </Button>
+      </ScrollView>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  input: {
+    marginVertical: 8,
+  },
+  headline: {
+    fontWeight: '700',
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  infoText: {
+    marginBottom: 12,
+  },
+  sendButton: {
+    marginTop: 12,
+  },
+});
 
 export { Feedback };
