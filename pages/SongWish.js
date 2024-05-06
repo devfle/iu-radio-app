42b7607cd56e70d7250eb3248170455a18459d3b
devfle <52854338+devfle@users.noreply.github.com>
2024-05-06 19:47:09 +0200

    feat: add content to song wish page

--- pages/SongWish.js
+++ pages/SongWish.js
@@ -1,7 +1,59 @@
-import { Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+import { TextInput, Button, Snackbar, Appbar } from 'react-native-paper';
+import { CurrentSong } from '../components';
 
 function SongWish() {
-  return <Text>SongWish Page!</Text>;
+  const [showSnack, setShowSnack] = React.useState(false);
+
+  const sendSongWish = () => {
+    setShowSnack(true);
+  };
+
+  return (
+    <>
+      <Appbar.Header mode="small">
+        <Appbar.Content title="Wish a song" />
+      </Appbar.Header>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.infoText}>
+          Do you want to say hello to your family or friends or simply want to listen to your favorite song? Then you've come to the right place! Just fill out the form and we will try to include your
+          song or greeting in our program.
+        </Text>
+        <TextInput style={styles.input} label="Song name"></TextInput>
+        <TextInput style={styles.input} label="Artist"></TextInput>
+        <TextInput style={{ ...styles.input, minHeight: 120 }} label="Personal message"></TextInput>
+        <Button style={styles.sendButton} icon="send" mode="contained" onPress={sendSongWish}>
+          Send request
+        </Button>
+      </ScrollView>
+      <Snackbar onDismiss={() => setShowSnack(false)} visible={showSnack}>
+        Thank you for your request!
+      </Snackbar>
+      <CurrentSong />
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  input: {
+    marginVertical: 8,
+  },
+  headline: {
+    fontWeight: '700',
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  infoText: {
+    marginBottom: 12,
+  },
+  sendButton: {
+    marginTop: 12,
+  },
+});
 
 export { SongWish };
