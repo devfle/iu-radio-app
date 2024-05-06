dbb596d2d2aad8248d65b50521e0e91aefde79ef
devfle <52854338+devfle@users.noreply.github.com>
2024-05-06 19:46:39 +0200

    feat: add content to home page

--- pages/Home.js
+++ pages/Home.js
@@ -1,76 +1,83 @@
 import React from 'react';
-import { Text, View, ScrollView, StyleSheet, Image } from 'react-native';
-import { Card, IconButton } from 'react-native-paper';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Card, Snackbar, Appbar } from 'react-native-paper';
+import { CurrentSong, Rating } from '../components';
 
 function Home() {
-  const [musicPlay, setMusicPlay] = React.useState(false);
-  const musicBarIcon = musicPlay ? 'play' : 'pause';
+  const [showSnack, setShowSnack] = React.useState(true);
+
+  const sendFeedback = () => {
+    setShowSnack(true);
+  };
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <Card style={{ ...styles.container, ...styles.card }}>
-        <Card.Title title="Rate the moderator"></Card.Title>
-        <Card.Content>
-          <View style={styles.preview}>
-            <Card.Cover
-              style={styles.thumbnail}
-              source={{
-                uri: 'https://images.unsplash.com/photo-1581368135153-a506cf13b1e1?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              }}
-            />
-            <View style={styles.previewText}>
-              <Text style={styles.cardHeadline}>John Smith</Text>
-              <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</Text>
+    <>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Appbar.Header mode="small">
+          <Appbar.Content title="Welcome to FooBarFM" />
+        </Appbar.Header>
+        <Card style={styles.card}>
+          <Card.Title style={styles.cardTitle} title="Rate the moderator" />
+          <Card.Content>
+            <View style={styles.preview}>
+              <Card.Cover
+                style={styles.thumbnail}
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1581368135153-a506cf13b1e1?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                }}
+              />
+              <View style={styles.previewText}>
+                <Text style={styles.cardHeadline}>John Smith</Text>
+                <Text>What is your opinion on the moderator? Let us know now and help us to improve.</Text>
+              </View>
             </View>
-          </View>
-        </Card.Content>
-        <Card.Actions></Card.Actions>
-      </Card>
+          </Card.Content>
+          <Card.Actions>
+            <Card.Actions>
+              <Rating onPress={sendFeedback} />
+            </Card.Actions>
+          </Card.Actions>
+        </Card>
 
-      <Card style={styles.card}>
-        <Card.Title title="Rate the playlist"></Card.Title>
-        <Card.Content>
-          <View style={styles.preview}>
-            <Card.Cover
-              style={styles.thumbnail}
-              source={{
-                uri: 'https://images.unsplash.com/photo-1581368135153-a506cf13b1e1?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              }}
-            />
-            <View style={styles.previewText}>
-              <Text style={styles.cardHeadline}>John Smith</Text>
-              <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</Text>
+        <Card style={styles.card}>
+          <Card.Title style={styles.cardTitle} title="Rate the playlist" />
+          <Card.Content>
+            <View style={styles.preview}>
+              <Card.Cover
+                style={styles.thumbnail}
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1619983081593-e2ba5b543168?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                }}
+              />
+              <View style={styles.previewText}>
+                <Text style={styles.cardHeadline}>Focus Beats</Text>
+                <Text>Do you like the playlist? Let us know what you think!</Text>
+              </View>
             </View>
-          </View>
-        </Card.Content>
-        <Card.Actions></Card.Actions>
-      </Card>
-      <View style={playStyles.container}>
-        <View style={playStyles.actorContainer}>
-          <Image
-            style={playStyles.thumbnail}
-            source={{
-              uri: 'https://images.unsplash.com/photo-1581368135153-a506cf13b1e1?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            }}
-          />
-          <Text style={playStyles.actorName}>John Smith - Song Name</Text>
-        </View>
-        <IconButton icon={musicBarIcon} size={20} onPress={() => setMusicPlay(!musicPlay)} />
-      </View>
-    </ScrollView>
+          </Card.Content>
+          <Card.Actions>
+            <Rating onPress={sendFeedback} />
+          </Card.Actions>
+        </Card>
+      </ScrollView>
+      <CurrentSong />
+      <Snackbar onDismiss={() => setShowSnack(false)} visible={showSnack}>
+        Thank you for your feedback!
+      </Snackbar>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  cardTitle: {
+    fontWeight: '700',
+  },
   scrollContainer: {
     flexGrow: 1,
   },
-  container: {
-    marginTop: 52,
-    marginBottom: 16,
-  },
   card: {
     marginHorizontal: 16,
+    marginVertical: 12,
   },
   preview: {
     flexDirection: 'row',
@@ -86,34 +93,6 @@ const styles = StyleSheet.create({
   cardHeadline: {
     marginBottom: 8,
     fontWeight: '700',
-  },
-});
-
-const playStyles = StyleSheet.create({
-  container: {
-    backgroundColor: '#dedede',
-    height: 50,
-    position: 'absolute',
-    bottom: 0,
-    zIndex: 10,
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 12,
-  },
-  actorContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  thumbnail: {
-    marginRight: 12,
-    height: 40,
-    width: 40,
-    borderRadius: 8,
-  },
-  actorName: {
-    fontWeight: '600',
   },
 });
 
